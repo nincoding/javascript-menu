4ef11749dc500de80f9a250403039fa7833ed514
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-01-13 22:47:10 +0900

    feat: 입력한 코치 이름의 유효성 검사 기능구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+const InputView = require('./views/InputView');
+const OutputView = require('./views/OutputView');
+const CoachNameValidator = require('./validators/CoachNameValidator');
 
 class App {
+
   play() {
-	
+	  OutputView.printServiceStartMessage();
+    this.inputCoachNames();
+  }
+
+  inputCoachNames() {
+	  InputView.readCoachNames(coachNames => {
+		  if(this.isValidInputValue(CoachNameValidator, coachNames)) {
+        this.inputHateMenus(coachNames.split(','));
+        return;
+      }
+
+      this.inputCoachNames();
+	  })
+  }
+
+  inputHateMenus(coachNameList) {
+    for (const coachName of coachNameList) {
+      InputView.readHateMenus(coachName);
+    }
+  }
+
+
+  isValidInputValue(validator, inputValue) {
+    try{
+      validator.validate(inputValue);
+      return true;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return false;
+    }
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -2,11 +2,15 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const InputView = {
 
-  readCoachNames (callback) {
+  readCoachNames(callback) {
     Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', 
     coachNames => callback(coachNames));
   },
 
+
+  readHateMenus() {
+
+  },
 }
 
 module.exports = InputView;
